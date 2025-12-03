7ca36c4a3c404fa6fa541ccf2f29507a32af266e
juno001-sys <juno001@kurashima.asia>
2025-12-03 12:05:01 +0900

    Update report.py

--- views/report.py
+++ views/report.py
@@ -1,22 +1,18 @@
 from flask import Blueprint
 
-report_bp = Blueprint("report", __name__, url_prefix="/reports")
-
+report_bp = Blueprint("report", __name__, url_prefix="/report")
 
 @report_bp.route("/usage")
 def usage_report():
     return "usage report"
 
-
 @report_bp.route("/cost")
 def cost_report():
     return "cost report"
 
-
 @report_bp.route("/suppliers")
 def suppliers_master():
     return "suppliers master"
-
 
 @report_bp.route("/items")
 def items_master():
