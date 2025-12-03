365481a9b77cf23a046e1e18afe82799586e7b10
juno001-sys <juno001@kurashima.asia>
2025-12-03 12:01:02 +0900

    Update stock.py

--- views/stock.py
+++ views/stock.py
@@ -1,8 +1,8 @@
-
 from flask import Blueprint
 
-stock_bp = Blueprint("stock", __name__, url_prefix="/stock")
+stock_bp = Blueprint("stock", __name__, url_prefix="/inventory")
 
-@stock_bp.route("/")
-def index():
-    return "stock screen"
+
+@stock_bp.route("/count")
+def inventory_count():
+    return "inventory count screen"
