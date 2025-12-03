33e84a1346714a1e9335f8bb260ab8369631ea97
juno001-sys <juno001@kurashima.asia>
2025-12-03 12:04:24 +0900

    Update purchase.py

--- views/purchase.py
+++ views/purchase.py
@@ -1,15 +1,7 @@
 from flask import Blueprint
 
-purchase_bp = Blueprint("purchase", __name__, url_prefix="/purchases")
+purchase_bp = Blueprint("purchase", __name__, url_prefix="/purchase")
 
-
-# 仕入れ入力（home.html の new_purchase に合わせる）
 @purchase_bp.route("/new")
 def new_purchase():
-    return "new purchase screen"
-
-
-# 仕入れレポート（home.html の purchase_report）
-@purchase_bp.route("/report")
-def purchase_report():
-    return "purchase report"
+    return "new purchase"
