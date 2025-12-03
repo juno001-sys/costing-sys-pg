3e7051d8e540af8e0f45a4ae73d8d508aeeb57d5
juno001-sys <juno001@kurashima.asia>
2025-12-03 10:48:24 +0900

    Update app.py

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ def create_app():
     # ---- ホーム ----
     @app.route("/")
     def home():
-        return render_template("home2.html")
+        return render_template("home.html")
 
     return app
 
