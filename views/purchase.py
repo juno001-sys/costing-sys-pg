91e442b82504c8c3e0c4095ed5512a601ce34c41
juno001-sys <juno001@kurashima.asia>
2025-12-03 11:41:56 +0900

    Update purchase.py

--- views/purchase.py
+++ views/purchase.py
@@ -1,4 +1,3 @@
-
 from flask import Blueprint, render_template
 
 purchase_bp = Blueprint("purchase", __name__, url_prefix="/purchases")
@@ -6,3 +5,11 @@ purchase_bp = Blueprint("purchase", __name__, url_prefix="/purchases")
 @purchase_bp.route("/")
 def index():
     return "purchase screen"
+
+
+# ---------------------------------------
+# home.html が呼んでいる old endpoint 互換
+# ---------------------------------------
+@purchase_bp.route("/new", endpoint="new_purchase")
+def new_purchase():
+    return index()
