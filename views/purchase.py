065cc115848b10a9d7f6b30a7058f52e385f7027
juno001-sys <juno001@kurashima.asia>
2025-12-03 10:45:33 +0900

    Update purchase.py

--- views/purchase.py
+++ views/purchase.py
@@ -1,1 +1,8 @@
 
+from flask import Blueprint, render_template
+
+purchase_bp = Blueprint("purchase", __name__, url_prefix="/purchases")
+
+@purchase_bp.route("/")
+def index():
+    return "purchase screen"
