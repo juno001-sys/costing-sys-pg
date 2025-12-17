f97991697e475e1b35f78766e024aeebc6a52328
juno001-sys <juno001@kurashima.asia>
2025-12-17 12:49:37 +0900

    Delete static/inventory_collapse.js

--- static/inventory_collapse.js
+++
@@ -1,7 +0,0 @@
-document.querySelectorAll("[data-toggle]").forEach((h) => {
-  h.addEventListener("click", (e) => {
-    if (e.target.closest("button")) return; // don't toggle when clicking buttons
-    const section = h.closest(".section");
-    section.classList.toggle("open");
-  });
-});
