820c797d78b3908b507f181f80dad0e6c2b59475
Juno Kurashima <junokurashima@Kurajikas-MacBook-Air-2022.local>
2026-01-08 18:19:33 +0900

    Show item temp zone/area/shelf on purchase form via API

--- views/loc/__init__.py
+++ views/loc/__init__.py
@@ -2,6 +2,7 @@
 
 from .locations_page import init_location_page
 from .locations_api import init_location_api
+from .item_location_api import init_item_location_api
 from .admin_store_config import init_admin_store_config
 from .locations_actions import init_location_actions
 from .shelves_page import init_location_shelves_page
@@ -18,6 +19,7 @@ def init_location_views(app, get_db):
     """
     init_location_page(app, get_db)
     init_location_api(app, get_db)
+    init_item_location_api(app, get_db)
     init_admin_store_config(app, get_db)
     init_location_actions(app, get_db)
     init_location_shelves_page(app, get_db)
